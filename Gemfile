d09f4e1e09ac71e1c7a290d710a602d818e85b2a
Gabriel Rocha <gabrielras12@hotmail.com>
2022-12-14 12:18:26 -0300

    Adicionar template argon

--- Gemfile
+++ Gemfile
@@ -51,7 +51,6 @@ gem "sassc-rails"
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
 
-
 gem 'audited', '~> 5.0'
 gem 'cocoon'
 gem 'cpf_cnpj', '~> 0.2.1'
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,14 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import * as bootstrap from "bootstrap"
+import "bootstrap"
+
+import "./src/jquery"
+import 'jquery-ui'
+
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {  
+  return new bootstrap.Popover(popoverTriggerEl)  
+})
+
+import './template/bootstrap.bundle.min.js'
